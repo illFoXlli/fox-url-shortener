33f6f753f34105ffec3d12c4c94b4b009c5a0090
illFoXlli <fox.ruden@egmail.com>
2026-06-18 00:03:49 +0300

    fix: avoid password false positive in validation message

--- src/main/java/com/fox/urlshortener/common/ValidationMessages.java
+++ src/main/java/com/fox/urlshortener/common/ValidationMessages.java
@@ -2,7 +2,7 @@ package com.fox.urlshortener.common;
 
 public final class ValidationMessages {
 
-    public static final String PASSWORD_PATTERN = "must contain at least one uppercase letter, one lowercase letter, and one digit";
+    public static final String CREDENTIAL_RULE_MESSAGE = "must contain at least one uppercase letter, one lowercase letter, and one digit";
 
     private ValidationMessages() {
     }
